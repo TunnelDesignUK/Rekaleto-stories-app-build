1ef8bdd7fecd6fa41d3a73d4542a8e2d3af4a3e1
drnoir <chrispgodber@gmail.com>
2023-07-21 13:53:23 +0100

    - updated lateste working app build

--- src/custum.js
+++ src/custum.js
@@ -1,61 +1,33 @@
+const fs = import('fs');
+const path = import('path')
+
 StartGen();
 
 async function StartGen(){
     await genList();
-    await CreateList('2019.json');
-    await CreateList('2020.json');
-    await CreateList('2021.json');
+    await CreateList('games.json');
 }
 
 // gen list
 function genList(){
-var fs = require('fs');
-var path = require('path');
-
-var diretoryTreeToObj = function(dir, done) {
-    var results = [];
-
-    fs.readdir(dir, function(err, list) {
-        if (err)
-            return done(err);
-
-        var pending = list.length;
-
-        if (!pending)
-            return done(null, {name: path.basename(dir), type: 'folder', children: results});
-
-        list.forEach(function(file) {
-            file = path.resolve(dir, file);
-            fs.stat(file, function(err, stat) {
-                if (stat && stat.isDirectory()) {
-                    diretoryTreeToObj(file, function(err, res) {
-                        results.push({
-                            name: path.basename(file),
-                            type: 'folder',
-                            children: res
-                        });
-                        if (!--pending)
-                            done(null, results);
-                    });
-                }
-                else {
-                    results.push({
-                        type: 'file',
-                        name: path.basename(file)
-                    });
-                    if (!--pending)
-                        done(null, results);
-                }
-            });
-        });
+    const jsonsInDir = fs.readdirSync('games').filter(file => path.extname(file) === '.json');
+    jsonsInDir.forEach(file => {
+      const fileData = fs.readFileSync(path.join('games', file));
+      const json = JSON.parse(fileData.toString());
     });
-};
+}
+
+function listItem (linkItem){
+        let li = document.createElement('li');
+        li.textContent = linkItem;
+        return li;
 }
 
 function CreateList(list){
     const listContainer = document.getElementById('Gamelist');
     list.forEach(link => {
-        listContainer.appendChild(<li a href ={link}>{linkTitle}</li>)
+        let newListItem = listItem(link);   
+        listContainer.appendChild(newListItem)
     });
 }
 
